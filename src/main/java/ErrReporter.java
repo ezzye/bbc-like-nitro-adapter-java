89950c3ccaf35e058b1a58e3d47cb150bc683a04
Errol Elliott <ezzy.elliott@gmail.com>
2016-06-24 22:24:46 +0100

    expanded pom for maven

--- src/main/java/ErrReporter.java
+++ src/main/java/ErrReporter.java
@@ -1,7 +1,28 @@
+import com.google.common.collect.ConcurrentHashMultiset;
+import com.google.common.collect.Multiset;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by ellioe03 on 23/06/2016.
  */
 public class ErrReporter implements Runnable {
+    private static final int MAX_ERR_COUNT = 10;
+    private static final int MAX_ERR_MSG_LENGTH = 100;
+    private static final Logger LOG = LoggerFactory.getLogger(ErrReporter.class);
+    private final String ispyer;
+    private final String ispyPrefix;
+    private volatile Multiset<String> errs;
+
+    public ErrReporter(String ispyer, String ispyPrefix, Multiset<String> errs) {
+        this.ispyer = ispyer;
+        this.ispyPrefix = ispyPrefix;
+        this.errs = ConcurrentHashMultiset.create();
+    }
+
+    public void adderror(String partName, String errMsg){
+        this.adderror(String.format("%s: %s",partName, errMsg), errMsg);
+    }
 
     @Override
     public void run() {
