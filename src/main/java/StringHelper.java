3a0860f26b60b53723aa21406228fd51aca35e2f
Ezzy Elliott <ezzy.elliott@gmail.com>
2016-06-25 15:42:12 +0100

    about to rain so I better save what I have

---
+++ src/main/java/StringHelper.java
@@ -0,0 +1,11 @@
+/**
+ * Created by errolelliott on 25/06/2016.
+ */
+public final class StringHelper {
+    private static final String ABBREVIATE_SUFFIX = "...";
+
+    public StringHelper() {
+    }
+
+    
+}
